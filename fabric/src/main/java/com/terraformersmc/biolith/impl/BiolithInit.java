8244d546750c0a8c771ba58b72d33224f3f511a3
gniftygnome <minecraft@gnifty.net>
2025-04-16 17:58:01 -0700

    Use Fabric/Forge/Neoforge event APIs for resource reloading.
    This has been necessitated in 1.21.4 by Neoforge arrogantly forcing modders not to use mixins to do this like we have all done just fine for a long time, and at the same time, changing their reloader event class, breaking any attempt to maintain cross-version mods.
    
    - Use Fabric/Forge/Neoforge event APIs for resource reloading

--- fabric/src/main/java/com/terraformersmc/biolith/impl/BiolithInit.java
+++ fabric/src/main/java/com/terraformersmc/biolith/impl/BiolithInit.java
@@ -2,8 +2,13 @@ package com.terraformersmc.biolith.impl;
 
 import com.terraformersmc.biolith.impl.biome.BiomeCoordinator;
 import com.terraformersmc.biolith.impl.compat.BiolithCompat;
+import com.terraformersmc.biolith.impl.data.BiomePlacementLoader;
+import com.terraformersmc.biolith.impl.data.IdentifiableResourceReloaderWrapper;
+import com.terraformersmc.biolith.impl.data.SurfaceGenerationLoader;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.minecraft.resource.ResourceType;
 
 public class BiolithInit implements ModInitializer {
     @Override
@@ -17,6 +22,10 @@ public class BiolithInit implements ModInitializer {
             // Watch for server events so we can maintain our status data.
             ServerLifecycleEvents.SERVER_STARTING.register(BiomeCoordinator::handleServerStarting);
             ServerLifecycleEvents.SERVER_STOPPED.register(BiomeCoordinator::handleServerStopped);
+
+            // Implement our resource reloaders The Fabric Way (tm).
+            ResourceManagerHelper.get(ResourceType.SERVER_DATA).registerReloadListener(new IdentifiableResourceReloaderWrapper("biome_placement_loader", new BiomePlacementLoader()));
+            ResourceManagerHelper.get(ResourceType.SERVER_DATA).registerReloadListener(new IdentifiableResourceReloaderWrapper("surface_generation_loader", new SurfaceGenerationLoader()));
 
             // Call loader-agnostic init.
             Biolith.init();
--- forge/src/main/java/com/terraformersmc/biolith/impl/BiolithInit.java
+++ forge/src/main/java/com/terraformersmc/biolith/impl/BiolithInit.java
@@ -1,7 +1,10 @@
 package com.terraformersmc.biolith.impl;
 
 import com.terraformersmc.biolith.impl.biome.BiomeCoordinator;
+import com.terraformersmc.biolith.impl.data.BiomePlacementLoader;
+import com.terraformersmc.biolith.impl.data.SurfaceGenerationLoader;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.AddReloadListenerEvent;
 import net.minecraftforge.event.server.ServerAboutToStartEvent;
 import net.minecraftforge.event.server.ServerStoppedEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -14,6 +17,10 @@ public class BiolithInit {
         // Watch for server events so we can maintain our status data.
         MinecraftForge.EVENT_BUS.addListener((ServerAboutToStartEvent event) -> BiomeCoordinator.handleServerStarting(event.getServer()));
         MinecraftForge.EVENT_BUS.addListener((ServerStoppedEvent event) -> BiomeCoordinator.handleServerStopped(event.getServer()));
+
+        // Implement our resource reloaders The Forge Way (tm).
+        MinecraftForge.EVENT_BUS.addListener((AddReloadListenerEvent event) -> event.addListener(new BiomePlacementLoader()));
+        MinecraftForge.EVENT_BUS.addListener((AddReloadListenerEvent event) -> event.addListener(new SurfaceGenerationLoader()));
 
         // Call loader-agnostic init.
         Biolith.init();

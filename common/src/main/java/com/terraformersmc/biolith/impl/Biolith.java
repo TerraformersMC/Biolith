776d3286d20e200a6c9b0abbbb0fb46b8a6aac65
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-04-22 19:28:56 -0400

    added config

--- common/src/main/java/com/terraformersmc/biolith/impl/Biolith.java
+++ common/src/main/java/com/terraformersmc/biolith/impl/Biolith.java
@@ -2,6 +2,7 @@ package com.terraformersmc.biolith.impl;
 
 import com.terraformersmc.biolith.impl.biome.BiomeCoordinator;
 import com.terraformersmc.biolith.impl.compat.BiolithCompat;
+import com.terraformersmc.biolith.impl.config.BiolithConfigManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,12 +10,17 @@ public class Biolith {
     public static final String MOD_ID = "biolith";
     public static final String MOD_NAME = "Biolith";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    private static final BiolithConfigManager CONFIG_MANAGER = new BiolithConfigManager();
 
     public static void init() {
         // Load the general config if it hasn't been loaded already
-        //CONFIG_MANAGER.getGeneralConfig();
+        CONFIG_MANAGER.getGeneralConfig();
 
         BiolithCompat.init();
         //BiolithCommands.init();
+    }
+
+    public static BiolithConfigManager getConfigManager() {
+        return CONFIG_MANAGER;
     }
 }

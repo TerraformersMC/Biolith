161875e0ca5d55be4de18409fb78cbf6f3136f59
gniftygnome <minecraft@gnifty.net>
2025-09-10 14:06:55 -0700

    Move resilient placed feature indexer to WrapMethod so Lithostitched can't bypass it.
    - Feature indexer work-around for Lithostitched

---
+++ common/src/main/java/com/terraformersmc/biolith/impl/mixin/MixinPlacedFeatureIndexer.java
@@ -0,0 +1,36 @@
+package com.terraformersmc.biolith.impl.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.terraformersmc.biolith.impl.Biolith;
+import com.terraformersmc.biolith.impl.feature.ResilientPlacedFeatureIndexer;
+import net.minecraft.registry.entry.RegistryEntryList;
+import net.minecraft.world.gen.feature.PlacedFeature;
+import net.minecraft.world.gen.feature.util.PlacedFeatureIndexer;
+import org.spongepowered.asm.mixin.Mixin;
+
+import java.util.List;
+import java.util.function.Function;
+
+@Mixin(PlacedFeatureIndexer.class)
+public class MixinPlacedFeatureIndexer {
+    @WrapMethod(method = "collectIndexedFeatures")
+    private static <T> List<PlacedFeatureIndexer.IndexedFeatures> biolith$wrapFeatureIndexer(List<T> biomes, Function<T, List<RegistryEntryList<PlacedFeature>>> biomesToPlacedFeaturesList, boolean listInvolvedBiomesOnFailure, Operation<List<PlacedFeatureIndexer.IndexedFeatures>> original) {
+        List<PlacedFeatureIndexer.IndexedFeatures> features;
+
+        if (Biolith.getConfigManager().getGeneralConfig().forceResilientFeatureIndexer()) {
+            Biolith.LOGGER.info("Using Biolith's resilient feature indexer (force_resilient_feature_indexer = true).");
+            features = ResilientPlacedFeatureIndexer.collectIndexedFeatures(biomes, biomesToPlacedFeaturesList);
+        } else {
+            try {
+                features = original.call(biomes, biomesToPlacedFeaturesList, listInvolvedBiomesOnFailure);
+            } catch (IllegalStateException e) {
+                Biolith.LOGGER.warn(e.getMessage());
+                Biolith.LOGGER.warn("Vanilla feature indexer failed; retrying with Biolith's resilient feature indexer...");
+                features = ResilientPlacedFeatureIndexer.collectIndexedFeatures(biomes, biomesToPlacedFeaturesList);
+            }
+        }
+
+        return features;
+    }
+}

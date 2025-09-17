3b221674c4dc2fe0851852592797d45c25bee1f8
gniftygnome <minecraft@gnifty.net>
2025-09-16 18:01:57 -0700

    Don't handle vanilla exception during recursive calls of placed feature indexer.
    - Fix soft lock of vanilla feature indexer (resolves #34)

--- common/src/main/java/com/terraformersmc/biolith/impl/mixin/MixinPlacedFeatureIndexer.java
+++ common/src/main/java/com/terraformersmc/biolith/impl/mixin/MixinPlacedFeatureIndexer.java
@@ -8,12 +8,16 @@ import net.minecraft.registry.entry.RegistryEntryList;
 import net.minecraft.world.gen.feature.PlacedFeature;
 import net.minecraft.world.gen.feature.util.PlacedFeatureIndexer;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 
 import java.util.List;
 import java.util.function.Function;
 
 @Mixin(PlacedFeatureIndexer.class)
 public class MixinPlacedFeatureIndexer {
+    @Unique
+    private static final ThreadLocal<Boolean> BIOLITH_RECURSION = ThreadLocal.withInitial(() -> false);
+
     @WrapMethod(method = "collectIndexedFeatures")
     private static <T> List<PlacedFeatureIndexer.IndexedFeatures> biolith$wrapFeatureIndexer(List<T> biomes, Function<T, List<RegistryEntryList<PlacedFeature>>> biomesToPlacedFeaturesList, boolean listInvolvedBiomesOnFailure, Operation<List<PlacedFeatureIndexer.IndexedFeatures>> original) {
         List<PlacedFeatureIndexer.IndexedFeatures> features;
@@ -21,7 +25,11 @@ public class MixinPlacedFeatureIndexer {
         if (Biolith.getConfigManager().getGeneralConfig().forceResilientFeatureIndexer()) {
             Biolith.LOGGER.info("Using Biolith's resilient feature indexer (force_resilient_feature_indexer = true).");
             features = ResilientPlacedFeatureIndexer.collectIndexedFeatures(biomes, biomesToPlacedFeaturesList);
+        } else if (BIOLITH_RECURSION.get()) {
+            // The original is recursive; don't catch its exceptions when it's calling itself.
+            features = original.call(biomes, biomesToPlacedFeaturesList, listInvolvedBiomesOnFailure);
         } else {
+            BIOLITH_RECURSION.set(true);
             try {
                 features = original.call(biomes, biomesToPlacedFeaturesList, listInvolvedBiomesOnFailure);
             } catch (IllegalStateException e) {
@@ -29,6 +37,7 @@ public class MixinPlacedFeatureIndexer {
                 Biolith.LOGGER.warn("Vanilla feature indexer failed; retrying with Biolith's resilient feature indexer...");
                 features = ResilientPlacedFeatureIndexer.collectIndexedFeatures(biomes, biomesToPlacedFeaturesList);
             }
+            BIOLITH_RECURSION.set(false);
         }
 
         return features;

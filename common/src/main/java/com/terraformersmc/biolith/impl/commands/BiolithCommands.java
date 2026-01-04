982a4af2c10d3283d433e6c8c15548536f786414
Rebel459 <131760345+Rebel459@users.noreply.github.com>
2026-01-04 14:35:01 +1100

    climate command

--- common/src/main/java/com/terraformersmc/biolith/impl/commands/BiolithCommands.java
+++ common/src/main/java/com/terraformersmc/biolith/impl/commands/BiolithCommands.java
@@ -1,18 +1,19 @@
 package com.terraformersmc.biolith.impl.commands;
 
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.terraformersmc.biolith.impl.Biolith;
 import com.terraformersmc.biolith.impl.platform.Services;
 
 import java.util.List;
 
 import static com.mojang.brigadier.arguments.StringArgumentType.word;
-import static net.minecraft.commands.Commands.argument;
-import static net.minecraft.commands.Commands.literal;
-import static net.minecraft.commands.arguments.EntityArgument.entity;
-import static net.minecraft.commands.arguments.coordinates.BlockPosArgument.blockPos;
+import static net.minecraft.command.argument.BlockPosArgumentType.blockPos;
+import static net.minecraft.command.argument.EntityArgumentType.entity;
+import static net.minecraft.server.command.CommandManager.*;
 
 public class BiolithCommands {
-    protected static List<String> COMMANDS = List.of("help", "describe");
+    protected static List<String> COMMANDS = List.of("help", "describe", "climate");
 
     public static void init() {
         if (!Biolith.getConfigManager().getGeneralConfig().areCommandsEnabled()) {
@@ -31,7 +32,15 @@ public class BiolithCommands {
                                 .then(argument("position", blockPos())
                                         .executes(BiolithDescribeCommand::atPosition)))
                         .executes(BiolithDescribeCommand::atCaller))
-                .executes(BiolithHelpCommand::noArgs))
+                .executes(BiolithHelpCommand::noargs)
+                .then(literal("climate")
+                        .then(literal("at")
+                                .then(argument("entity", entity())
+                                        .executes(BiolithClimateCommand::atEntity))
+                                .then(argument("position", blockPos())
+                                        .executes(BiolithClimateCommand::atPosition)))
+                        .executes(BiolithClimateCommand::atCaller))
+                )
         );
     }
 }

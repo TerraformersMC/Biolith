662f3c85f2c35732deddf8dea28b235033ffbd6e
gniftygnome <minecraft@gnifty.net>
2026-03-24 22:06:48 -0700

    Fix NeoForge mixin.
    - Fix NeoForge mixin.

--- neoforge/src/main/java/com/terraformersmc/biolith/impl/mixin/MixinSaveLoader.java
+++ neoforge/src/main/java/com/terraformersmc/biolith/impl/mixin/MixinSaveLoader.java
@@ -6,7 +6,7 @@ import net.minecraft.server.RegistryLayer;
 import net.minecraft.server.ReloadableServerResources;
 import net.minecraft.server.WorldStem;
 import net.minecraft.server.packs.resources.CloseableResourceManager;
-import net.minecraft.world.level.storage.WorldData;
+import net.minecraft.world.level.storage.LevelDataAndDimensions;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(value=WorldStem.class, priority = 500)
 public class MixinSaveLoader {
     @Inject(method = "<init>", at = @At(value = "INVOKE", target = "Ljava/lang/Record;<init>()V", shift = At.Shift.AFTER))
-    private void biolith$earlyCaptureRegistries(CloseableResourceManager lifecycledResourceManager, ReloadableServerResources dataPackContents, LayeredRegistryAccess<RegistryLayer> combinedDynamicRegistries, WorldData saveProperties, CallbackInfo ci) {
-        BiomeCoordinator.setRegistryManager(combinedDynamicRegistries);
+    private void biolith$earlyCaptureRegistries(CloseableResourceManager resourceManager, ReloadableServerResources dataPackResources, LayeredRegistryAccess<RegistryLayer> registries, LevelDataAndDimensions.WorldDataAndGenSettings worldDataAndGenSettings, CallbackInfo ci) {
+        BiomeCoordinator.setRegistryManager(registries);
     }
 }
